67f8071c38b3e2982315b954725040a1878eb4b8
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-05 15:59:34 +0530

    practiceing

--- day2.js
+++ day2.js
@@ -16,8 +16,41 @@
 // console.log(getFileExtention("teet.js"));
 
 //Problem :
-// program to get the extension of the filename
+// program to replace every character in a given string with the charecter following in it the alphabet
 //Solution :
-// const getFileExtention = (file) => file.split(".")[file.split(".").length - 1];
-// const getFileExtention = (file) => file.slice(file.lastIndexOf("."));
-// console.log(getFileExtention("teet.test.js"));
+// const getFileExtention = (str) =>
+//   str
+//     .split("")
+//     .map((char) =>
+//       String.fromCharCode(
+//         char.charCodeAt(0) === 122 //for z
+//           ? 97
+//           : char.charCodeAt(0) === 90 // for Z
+//           ? 65
+//           : char.charCodeAt(0) + 1
+//       )
+//     )
+//     .join("");
+// console.log(getFileExtention("abcz"));
+
+//Problem :
+// program to get the current date in mm-dd-yyyy, mm/dd/yyyy,dd/mm/yyyy
+//Solution :
+// const getDate = (date = new Date()) => {
+//   const days = date.getDate() + 1;
+//   const months = date.getMonth() + 1;
+//   const years = date.getFullYear();
+//   return `${days < 9 ? "0" + days : days}/${
+//     months < 9 ? "0" + months : months
+//   }/${years}`;
+// };
+// console.log(getDate());
+
+//Problem :
+// program to create a new string adding "New!" in the front of a giving string.If the given string begins with "New!" already then return the original
+//Solution :
+// const stringManipulation = (str) =>
+//   str.split(" ")[0] === "New!" ? str : "New! " + str;
+const stringManipulation = (str) =>
+  str.indexOf("New!") === 0 ? str : "New! " + str;
+console.log(stringManipulation("New! abcdsss Testing string"));
