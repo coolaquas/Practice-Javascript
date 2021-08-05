1677a7de79a0ecbbdfb68e8185a55c8d574e3a3d
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-05 15:02:08 +0530

    practiceing

---
+++ day2.js
@@ -0,0 +1,23 @@
+//Problem :
+// program to check to number and return true if one of the number is 100 or if the sum of the two number is 100
+//Solution :
+// const check100 = (a, b) => {
+//   if (a === 100 || b === 100 || a + b === 100) {
+//     return true;
+//   }
+//   return false;
+// };
+// console.log(check100(50, 50));
+
+//Problem :
+// program to get the extension of the filename
+//Solution :
+// const getFileExtention = (file) => file.split(".")[file.split(".").length - 1];
+// console.log(getFileExtention("teet.js"));
+
+//Problem :
+// program to get the extension of the filename
+//Solution :
+// const getFileExtention = (file) => file.split(".")[file.split(".").length - 1];
+// const getFileExtention = (file) => file.slice(file.lastIndexOf("."));
+// console.log(getFileExtention("teet.test.js"));
