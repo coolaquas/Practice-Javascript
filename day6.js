c479a79dd8d0ee856e3ddacb571d72d97a4ebfe6
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-10 22:51:23 +0530

    challenge 2

--- day6.js
+++ day6.js
@@ -1,5 +1,11 @@
 //Problem :
 // program to return a string in alphabetical order
 //Solution :
-const alphabatical_string = (str) => str.split("").sort().join("");
-console.log(alphabatical_string("webmaster"));
+// const alphabatical_string = (str) => str.split("").sort().join("");
+// console.log(alphabatical_string("webmaster"));
+
+//Problem :
+// program to count the number of vowels in the string
+//Solution :
+const alphabatical_string = (str) => str.match(/[aeiou]/gi).length;
+console.log(alphabatical_string("Samrat"));
