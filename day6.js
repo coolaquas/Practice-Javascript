0816310370ab2efaed83b9e322650508bd8177c8
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-10 23:02:03 +0530

    challenge 3

--- day6.js
+++ day6.js
@@ -7,5 +7,23 @@
 //Problem :
 // program to count the number of vowels in the string
 //Solution :
-const alphabatical_string = (str) => str.match(/[aeiou]/gi).length;
-console.log(alphabatical_string("Samrat"));
+// const alphabatical_string = (str) => str.match(/[aeiou]/gi).length;
+// console.log(alphabatical_string("Samrat"));
+
+//Problem :
+// program to convert an amount into notes and coins
+//Solution :
+const amountTocoins = (amount, coins) => {
+  if (amount === 0) {
+    return [];
+  } else {
+    if (amount >= coins[0]) {
+      left = amount - coins[0];
+      return [coins[0]].concat(amountTocoins(left, coins));
+    } else {
+      coins.shift();
+      return amountTocoins(amount, coins);
+    }
+  }
+};
+console.log(amountTocoins(46, [25, 10, 5, 2, 1]));
