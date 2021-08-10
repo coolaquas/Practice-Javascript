3f8a53b3ccca0a41b0046cba02e39399f600f4e6
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-10 22:45:12 +0530

    challenge 1

---
+++ day6.js
@@ -0,0 +1,5 @@
+//Problem :
+// program to return a string in alphabetical order
+//Solution :
+const alphabatical_string = (str) => str.split("").sort().join("");
+console.log(alphabatical_string("webmaster"));
