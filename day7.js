9af2d7fc5911404bdd4fd0789dfb54c45886fea1
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-11 11:25:30 +0530

    solvind kata

---
+++ day7.js
@@ -0,0 +1,18 @@
+//Problem :
+// program to get a number and return coresponding aschi code value
+// Solution :
+// const aschi_to_char = (num) => String.fromCharCode(num);
+// console.log(aschi_to_char(90));
+
+//Problem :
+// program to KATA sample fun theater problem
+// Solution :
+// const seatsInTheater = (nCols, nRows, col, row) =>
+//   (nCols - col + 1) * (nRows - row);
+// console.log(seatsInTheater(16, 11, 5, 3));
+
+//Problem :
+// program to get a number and return coresponding aschi code value
+// Solution :
+// const aschi_to_char = (num) => String.fromCharCode(num);
+// console.log(aschi_to_char(90));
