445cfb3e797e8bd5f1238f32f4b6abac6c4722f9
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-11 11:43:10 +0530

    kata done

--- day7.js
+++ day7.js
@@ -12,7 +12,13 @@
 // console.log(seatsInTheater(16, 11, 5, 3));
 
 //Problem :
-// program to get a number and return coresponding aschi code value
+// program to find the point in football championship
 // Solution :
-// const aschi_to_char = (num) => String.fromCharCode(num);
-// console.log(aschi_to_char(90));
+const points = (games) =>
+  games.reduce((acc, game) => {
+    const [x, y] = game.split(":");
+    return (acc += x > y ? 3 : x === y ? 1 : 0);
+  }, 0);
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:5"])
+);
