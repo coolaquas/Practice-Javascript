e0b46cd37fa6c3c1905fa91b3698448b14a0967a
Samrat Mallick <samrat.mallick1@gmail.com>
2021-08-24 14:52:07 +0530

    practicing

--- day17.js
+++ day17.js
@@ -50,8 +50,32 @@ const sol4 = (a = 0, b = 0, c = 0) => {
 /*Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front
  */
 //Solution
-const sol5 = (a = 0, b = 0, c = 0) => {
-  let s = (a + b + c) / 2;
-  console.log((s * (s - a) * (s - b) * (s - c)) ** 0.5);
+const sol5 = (str) => {
+  console.log(str.split("").reverse().join(""));
 };
-// sol5(3, 6, 7);
+// sol5("w3resource");
+
+/*Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar
+ */
+//Solution
+const sol6 = (year) => {
+  console.log(
+    year % 4 === 0 && year % 100 !== 0
+      ? year + " is a leap year"
+      : year % 400 === 0
+      ? year + " is a leap year"
+      : year + " is not a leap year"
+  );
+};
+// sol6(2003);
+
+/*Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050
+ */
+//Solution
+const sol7 = () => {
+  for (let i = 2014; i < 2050; i++) {
+    if (new Date(i, 0, 1).getDay() === 0)
+      console.log(`1st january, ${i} is a sunday.`);
+  }
+};
+// sol7();
